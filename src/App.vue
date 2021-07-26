64014e12c3b88c3666ffa70011e177a3d18bc6c7
leo <leoreich@posteo.de>
2021-07-26 13:59:17 +0200

    basic tree structure

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Hyperstructurization</h1>
+    <text-node :nodedata='flare'></text-node>
+  </div>
+</template>
+
+<script>
+import TextNode from './components/TextNode.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TextNode
+  },
+  data: function() {
+    return { flare: { content: "", children: []} }
+  },
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TextNode.vue
@@ -0,0 +1,44 @@
+<template>
+<div class='treelevel'>
+    <div v-for='(child, index) in nodedata.children' :key='index' class='textnode'>
+        <i @click="collapse">c</i>
+        <p class="editable" contenteditable="true">{{child.content ? child.content : '(empty)'}}</p>
+        <text-node v-if="!collapsed" :nodedata='child'></text-node>
+    </div>
+    <el-button @click="addNode">Add Node</el-button>
+</div>
+</template>
+
+<style>
+.textnode {
+    display: flex;
+}
+
+.treelevel {
+    margin-top: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-content: flex-start;
+}
+</style>
+
+<script>
+
+export default {
+    name: 'text-node',
+    props: {
+        nodedata: Object
+    },
+    data: () => ({
+        collapsed: true
+    }),
+    methods: {
+        addNode(){
+            this.nodedata.children.push({content: "", children: []})
+        },
+        collapse(){
+            this.collapsed = !this.collapsed
+        }
+    }
+}
+</script>
